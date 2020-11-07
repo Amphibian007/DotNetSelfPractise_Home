40d4b92fc1763ff22ed6ef4aa63620df89dce19d
Dipto Das <diptod111@gmail.com>
2020-11-07 11:44:21 +0600

    Adding Razor View and Layout codes

---
+++ source/Learning Never Ends/AllLayouts/MyLayout1/Controllers/FoodController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyLayout1.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult FoodIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/Learning Never Ends/AllLayouts/MyLayout1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyLayout1.Models;
+
+namespace MyLayout1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var food1 = new Food()
+            {
+                id = 1,
+                name = "Jackfruit",
+                age = 15
+            };
+            var food2 = new Food()
+            {
+                id = 2,
+                name = "banana",
+                age = 56
+            };
+            var food3 = new Food()
+            {
+                id = 3,
+                name = "Mango",
+                age = 34
+            };
+
+            List < Food > foodList= new List<Food>();
+            foodList.Add(food1);
+            foodList.Add(food2);
+            foodList.Add(food3);
+            return View(foodList);
+        }
+        public IActionResult Details(int id)
+        {
+            var food1 = new Food()
+            {
+                id = id,
+                name = "Jackfruit",
+                age = 15
+            };
+            return View(food1);
+        }
+    }
+}
