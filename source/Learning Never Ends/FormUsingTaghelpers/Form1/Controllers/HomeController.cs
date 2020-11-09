c41ac8644c2b04f1074fef34df914b6c739cd0e2
Dipto Das <diptod111@gmail.com>
2020-11-09 18:06:48 +0600

    Added form using taghelpers code

--- source/Learning Never Ends/FormUsingTaghelpers/Form1/Controllers/HomeController.cs
+++ source/Learning Never Ends/FormUsingTaghelpers/Form1/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Form1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Form1.Controllers
@@ -11,6 +12,12 @@ namespace Form1.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public string Index(Employee emp)
+        {
+            return ($"Name is {emp.Name} , age is {emp.Age} ,gender is{emp.Gender}" +
+                  $"designation is {emp.Designation} and marital status is {emp.Married}");
         }
     }
 }
---
+++ source/Learning Never Ends/ModelBinding/ModelBinding1_Project/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBinding1_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Details(int id, string name)
+        {
+            return $"Id is {id} and Name is {name}";
+        }
+    }
+}
