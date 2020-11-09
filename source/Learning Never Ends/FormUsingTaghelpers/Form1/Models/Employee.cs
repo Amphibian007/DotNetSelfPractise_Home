c8d309e507118ca54438ff3373d5e3a734d5b85a
Dipto Das <diptod111@gmail.com>
2020-11-09 13:12:52 +0600

    Added form using taghelpers code

---
+++ source/Learning Never Ends/FormUsingTaghelpers/Form1/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Form1.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+        public Gender Gender { get; set; }
+
+        public string Married { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Felmale
+    }
+}
