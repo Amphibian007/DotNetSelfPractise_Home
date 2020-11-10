d580d7182af2fc3294d02d4385c3ed16aec4f919
Dipto Das <diptod111@gmail.com>
2020-11-10 20:06:39 +0600

    Added Field validation in form2

--- source/Learning Never Ends/FormUsingTaghelpers/Form2/Models/Employee.cs
+++ source/Learning Never Ends/FormUsingTaghelpers/Form2/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace Form2.Models
 {
     public class Employee
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
         public Gender Gender { get; set; }
+        [Required]
         public string Designation { get; set; }
+        [Required]
         public double Salary { get; set; }
+        [Required]
         public string Married { get; set; }
 
     }
