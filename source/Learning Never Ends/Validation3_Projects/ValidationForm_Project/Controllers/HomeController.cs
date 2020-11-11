804559dbda9417155d89f4d5059944727651c109
Dipto Das <diptod111@gmail.com>
2020-11-11 23:56:36 +0600

    Tried all validations successfully

---
+++ source/Learning Never Ends/Validation3_Projects/ValidationForm_Project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ValidationForm_Project.Models;
+
+namespace ValidationForm_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(Employee emp)
+        {
+            var model = new Employee()
+            {
+                Name = "Dipto",
+                Gender = "Male"
+            };
+            if (ModelState.IsValid)
+            {
+                return View("Details", emp);
+            }
+            else
+                return View("Index");
+        }
+    }
+}
---
+++ source/Learning Never Ends/Validation3_Projects/ValidationForm_Project/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidationForm_Project.Models
+{
+    public class Employee
+    {
+        [Required (ErrorMessage ="This field is required")]
+        [MinLength(2)]
+        public string Name { get; set; }
+        [Required (ErrorMessage ="This field is also required")]
+        public string Gender { get; set; }
+
+        [Required (ErrorMessage ="Age is require")] 
+        [Range(10,maximum:15, ErrorMessage ="Enter age between in 10 to 15")]
+        public int? Age { get; set; }
+        [Required(ErrorMessage = "Email is require")]
+        [EmailAddress(ErrorMessage ="Email is not valid")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Password is required")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+        ,ErrorMessage = "Minimum eight characters, at least one uppercase , one lowercase and one number")]
+        public string Password { get; set; }
+        [Required(ErrorMessage ="Confirm password is a must")]
+        [Compare("Password")]
+        [Display(Name="Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage ="Channel Url is must")]
+        [Url(ErrorMessage ="Url is not correct")]
+        public string ChannelUrl { get; set; }
+    }
+}
