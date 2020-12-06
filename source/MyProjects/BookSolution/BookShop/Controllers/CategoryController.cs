84a67fb4d2ef7b446bb2f0d12c6d187608606fd7
Dipto Das <diptod111@gmail.com>
2020-12-06 14:56:00 +0600

    Added book shop project upto create category

---
+++ source/MyProjects/BookSolution/BookShop/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Data;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> myList = _db.Category;
+            return View(myList);
+        }
+        //Get-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ source/MyProjects/BookSolution/BookShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
