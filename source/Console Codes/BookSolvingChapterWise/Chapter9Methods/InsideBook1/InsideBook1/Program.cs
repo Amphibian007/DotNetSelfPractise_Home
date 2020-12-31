86af857051a56f3a44a508772ed5425a8b9beee5
Dipto Das <diptod111@gmail.com>
2020-12-31 11:54:25 +0600

    Added day 1 in Chapter 9 codes upto Passing of Expressions as Method Arguments

---
+++ source/Console Codes/BookSolvingChapterWise/Chapter9Methods/InsideBook1/InsideBook1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace InsideBook1
+{
+    class Program
+    {
+       
+        public static void ModifyArray(int[] passedArray)
+        {
+            passedArray[0] = 7;
+            PrintArray(passedArray);
+            
+        }
+        public static void PrintArray(int[] passedArray)
+        {
+            Console.Write("[");
+            if (passedArray.Length > 0)
+            {
+                Console.Write(passedArray[0]);
+                for(int i = 1; i < passedArray.Length; i++)
+                {
+                    Console.Write(passedArray[i]);
+                }
+            }
+            Console.WriteLine("]");
+                
+        }
+        static void Main(string[] args)
+        {
+            int[] myArr = { 1, 2, 3 };
+            Console.Write("Before passing the array");
+            PrintArray(myArr);
+            Console.WriteLine("After passing the array: ");
+            ModifyArray(myArr);
+            
+
+        }
+    }
+}
