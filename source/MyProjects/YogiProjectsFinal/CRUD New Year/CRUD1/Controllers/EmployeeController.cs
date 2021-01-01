55d6455dcf46523dbceab37f3020a017eefd0b27
Dipto Das <diptod111@gmail.com>
2021-01-01 14:42:26 +0600

    Added CRUD in New Year

---
+++ source/MyProjects/YogiProjectsFinal/CRUD New Year/CRUD1/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using CRUD1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Repositories.EmployeeRepository.iterateEmployees);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            Repositories.EmployeeRepository.Create(employee);
+            return View("Thanks", employee);
+        }
+        public IActionResult Update(string employeeName)
+        {
+            var employee = Repositories.EmployeeRepository.allEmployees.Where(e => e.Name == employeeName).FirstOrDefault();
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult Update(Employee employee,string employeeName)
+        {
+            Repositories.EmployeeRepository.allEmployees.Where(e => e.Name == employeeName).FirstOrDefault().Age=employee.Age;
+            Repositories.EmployeeRepository.allEmployees.Where(e => e.Name == employeeName).FirstOrDefault().Name=employee.Name;
+            Repositories.EmployeeRepository.allEmployees.Where(e => e.Name == employeeName).FirstOrDefault().Salary = employee.Salary ;
+            Repositories.EmployeeRepository.allEmployees.Where(e => e.Name == employeeName).FirstOrDefault().Department=employee.Department;
+            Repositories.EmployeeRepository.allEmployees.Where(e => e.Name == employeeName).FirstOrDefault().Gender=employee.Gender;
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Delete(string empName)
+        {
+            var employee = Repositories.EmployeeRepository.allEmployees.Where(e => e.Name == empName).FirstOrDefault();
+            Repositories.EmployeeRepository.Delete(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ source/MyProjects/YogiProjectsFinal/CRUD New Year/CRUD1/Repositories/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using CRUD1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD1.Repositories
+{
+    public static class EmployeeRepository
+    {
+        public static List<Employee> allEmployees = new List<Employee>();
+        public static IEnumerable<Employee> iterateEmployees
+        {
+            get { return allEmployees; }
+        }
+        public static void Create(Employee em1)
+        {
+            allEmployees.Add(em1);
+        }
+        public static void Delete(Employee em1)
+        {
+            allEmployees.Remove(em1);
+        }
+    }
+}
