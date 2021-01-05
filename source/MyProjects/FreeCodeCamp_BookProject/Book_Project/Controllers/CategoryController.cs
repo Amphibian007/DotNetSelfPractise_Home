f14344c652d9c55afcf5852792804800d79006ca
Dipto Das <diptod111@gmail.com>
2021-01-05 14:07:19 +0600

    Added FreeCodeCamp_BookProject upto read Part1

---
+++ source/MyProjects/FreeCodeCamp_BookProject/Book_Project/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Book_Project.DataAccessLayer;
+using Book_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Project.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly BookDbContext _bookDb;
+        public CategoryController(BookDbContext bookDb)
+        {
+            _bookDb = bookDb;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> bookList = _bookDb.Category;
+            return View(bookList);
+        }
+    }
+}
---
+++ source/MyProjects/FreeCodeCamp_BookProject/Book_Project/DataAccessLayer/BookDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Book_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Project.DataAccessLayer
+{
+    public class BookDbContext:DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
